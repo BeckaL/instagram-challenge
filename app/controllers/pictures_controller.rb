19273e187439e6b82c68b26bd233308f2679d0b6
Becka <rebecca.lelew@gmail.com>
2018-11-24 22:01:34 +0000

    Starts to add in picture upload capability - form submits but no save yet

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,14 @@
 class PicturesController < ApplicationController
-  
 
+  def create
+    @picture = Picture.new
+    redirect_to '/'
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:title, :caption, :image)
+  end
 
 end
