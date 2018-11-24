ca9879ff9bf07d2724def11ffa99322c11ae971b
Becka <rebecca.lelew@gmail.com>
2018-11-24 21:05:52 +0000

    Adds Picture model, controller and new picture form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_143201) do
+ActiveRecord::Schema.define(version: 2018_11_24_205455) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "title"
+    t.text "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_11_24_143201) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
