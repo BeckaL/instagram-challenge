fa3c782ae475f2af46cc876348eb6bdc6d0e801e
Becka <rebecca.lelew@gmail.com>
2018-12-02 17:38:27 +0000

    Improves site navigation with buttons

--- spec/features/upload_image_spec.rb
+++ spec/features/upload_image_spec.rb
@@ -4,12 +4,11 @@ describe 'upload picture' do
 
   it 'uploads a picture' do
     sign_up
-    visit '/pictures/new'
+    click_link 'Post new picture'
     fill_in :Title, with: 'test title'
     fill_in :Caption, with: 'test caption'
     attach_file(:Image, '/Users/rebeccalelew/Projects/instagram-challenge/spec/features/test_image.jpg')
     click_button("Create Picture")
-    visit '/pictures'
     expect(page).to have_content "test title"
     expect(page).to have_content "test caption"
   end
